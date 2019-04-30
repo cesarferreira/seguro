ae55cbdf6d1496192fb057b8f9d84e385c7c391d
Cesar Ferreira <cesar.manuel.ferreira@gmail.com>
2019-04-30 16:28:32 +0100

    added permission handling

--- sample/src/main/java/com/example/sample/MainActivity.kt
+++ sample/src/main/java/com/example/sample/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.sample
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import cesarferreira.library.Seguro
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import com.karumi.dexter.listener.single.PermissionListener
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,6 +33,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        checkPermissions()
+
 //        seguro.clear()
 
         // READ
@@ -33,8 +46,7 @@ class MainActivity : AppCompatActivity() {
         writeButton.setOnClickListener {
             seguro.Editor()
                 .put(TIME_KEY, Date().toString())
-                .put(TIME_KEY, Date().toString())
-                .put(TIME_KEY, Date().toString())
+                .put("name", "cesar ferreira")
                 .apply()
 
             textView.text = "I WROTE STUFF"
@@ -43,8 +55,24 @@ class MainActivity : AppCompatActivity() {
 //        Log.d("seguro", "" + seguro.total)
     }
 
+    private fun checkPermissions() {
+        Dexter.withActivity(this)
+            .withPermissions(
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ).withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport) {/* ... */
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permissions: List<PermissionRequest>,
+                    token: PermissionToken
+                ) {/* ... */
+                }
+            }).check()
+    }
+
     companion object {
-        val DEFAULT_DIRECTORY = "123"
         internal var TIME_KEY = "KEY_TIME"
 
     }
